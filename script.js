a78512d7bf63a270ba30eee169bdf4ccc19a8aec
Sijenyi96 <everlynsijenyi96@gmail.com>
2024-07-18 12:33:47 +0300

    update changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // setup event listener for page load
 document.addEventListener('DOMContentLoaded',() => {
-
+       loadTasks();
     // select DOM elements
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
